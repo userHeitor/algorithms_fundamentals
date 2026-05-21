132668081bdd066147663d8a5c2a6c224b8009ed
userHeitor <heitorbenigno.pessoal@gmail.com>
2026-05-21 03:53:17 -0300

    2

---
+++ algLista04/2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int N;
+	int A[N][N];
+	
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			if (i == j) A[i][j] = 1;
+			else A[i][j] = 0;
+		}
+	}
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			cout << A[i][j] << " ";
+		}
+		cout << endl;
+	}
+
+}
